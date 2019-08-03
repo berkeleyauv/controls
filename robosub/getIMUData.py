bd6eca40be0cc58670314e0211e92fbfbf835da3
Ashwin Vangipuram <ashwinvangipuram@gmail.com>
2019-08-03 08:42:30 -0700

    Competition code and data

--- robosub/getIMUData.py
+++ robosub/getIMUData.py
@@ -6,10 +6,40 @@ import rospy
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Imu
+from getYaw import yawl
+import cv2
+from datetime import datetime
 
 '''
 A python script to practice receiving ROS messages
 '''
+
+zed = cv2.VideoCapture(0)
+webcam = cv2.VideoCapture(1)
+# Define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+zed_video = cv2.VideoWriter('raw_video/zed_video_' + str(datetime.now()) + '.avi', fourcc, 30, (int(zed.get(3)), int(zed.get(4))))
+webcam_video = cv2.VideoWriter('raw_video/webcam_video_' + str(datetime.now()) + '.avi', fourcc, 30, (int(webcam.get(3)), int(webcam.get(4))))
+
+telemetry_file = 'telemetry_' + str(datetime.now()) + '.csv'
+telemetry_header = 'Time,Linear Acceleration X,Linear Acceleration Y,Linear Acceleration Z,Angular Velocity X,Angular Velocity Y,Angular Velocity Z,Orientation W,Orientation X,Orientation Y,Orientation Z,Yaw\n'
+with open(telemetry_file, 'a+') as file:
+    file.write(telemetry_header)
+
+
+def writeVideo():
+    zed_ret, zed_frame = zed.read()
+    if zed_ret:
+        zed_video.write(zed_frame)
+    webcam_ret, webcam_frame = webcam.read()
+    if webcam_ret:
+        webcam_video.write(webcam_frame)
+
+def releaseVideo():
+    zed.release()
+    zed_video.release()
+    webcam.release()
+    webcam_video.release()
 
 class IMUListener():
     ''' Subscribes to ROS messages
@@ -47,6 +77,14 @@ class IMUListener():
         self.linear_acceleration = msg.linear_acceleration
         self.angular_velocity = msg.angular_velocity
         self.orientation = msg.orientation
+
+        with open(telemetry_file, 'a+') as file:
+            data = [datetime.now(), msg.linear_acceleration.x, msg.linear_acceleration.y, msg.linear_acceleration.z,
+                    msg.angular_velocity.x, msg.angular_velocity.y, msg.angular_velocity.z, msg.orientation.w, msg.orientation.x,
+                    msg.orientation.y, msg.orientation.z, yawl.yaw]
+            data = ','.join(map(str, data))
+            file.write(data + '\n')
+        writeVideo()
         # print("Linear accel: {}".format(msg.linear_acceleration))
         # print("Angular vel: {}".format(msg.angular_velocity))
         # print("Orientation: {}".format(msg.orientation))
@@ -60,6 +98,7 @@ def integrate(arr):
         prevTime = time
         prevVal = val
     return total
+
 
 #rospy.init_node('imu_listener')
 imu = IMUListener()
