5a215025f6073e6ad6eb22857be4f774dcdf05c6
William Loo <williamloo888@gmail.com>
2020-03-13 01:43:36 -0700

    add razor imuserial  to rostopic code

---
+++ robosub/imu_to_rostopic.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import serial
+import rospy
+from std_msgs.msg import String
+
+ser = serial.Serial('/dev/ttyACM0')
+
+pub = rospy.Publisher('razor-imu', String, queue_size=10)
+
+def imu():
+  rospy.init_node('razor-imu', anonymous=True)
+  rate = rospy.Rate(10) # 10hz
+  while not rospy.is_shutdown():
+    output = str(ser.readline())
+    rospy.loginfo(output)
+    pub.publish(hello_str)
+    rate.sleep()
+
+if __name__ == '__main__':
+  try:
+    imu()
+  except rospy.ROSInterruptException:
+    pass
