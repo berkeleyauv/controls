a46befc6847635233853ef48227f3e790b5b2de1
Ashwin Vangipuram <ashwinvangipuram@gmail.com>
2020-04-26 23:12:51 -0400

    Fixed razor IMU and made into a threaded node

--- robosub/RazorIMU.py
+++ robosub/RazorIMU.py
@@ -8,20 +8,31 @@ from threading import Thread
 class RazorIMU(Thread):
 
     def __init__(self, read_frequency=10, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.serial = serial.Serial('/dev/ttyACM0')
+        super(RazorIMU, self).__init__(*args, **kwargs)
+        self.serial = serial.Serial('/dev/ttyACM1')
         self.pub = rospy.Publisher('/controls/razor', String, queue_size=10)
         ## TODO: Add more topics that we publish to individually
         self.rate = rospy.Rate(read_frequency) # 10hz
   
     def run(self):
-        output = str(self.serial.readline())
-        rospy.loginfo(output)
-        self.reading = output
-        self.pub.publish(output)
+        while not rospy.is_shutdown():
+            output = str(self.serial.readline())
+            vals = output.replace(' ', '').split(',')
+            assert len(vals) == 18
+            self.time = vals[0]
+            self.accel = vals[1:4]
+            self.gyro = vals[4:7]
+            self.mag = vals[7:10]
+            self.quat = vals[10:14]
+            self.angle = vals[14:17]
+            self.heading = vals[17]
+            self.reading = output
+            self.pub.publish(output)
+            rospy.loginfo(output)
 
-
-razor = RazorIMU(daemon=True)
+rospy.init_node('razor_imu')
+razor = RazorIMU()
+razor.daemon = True
 razor.start()
 
 
