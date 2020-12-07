0efd38ae545725a0f5cc57834a3fef74265fcea1
Marco Gellecanao <marcogelle@berkeley.edu>
2020-12-06 17:41:19 -0800

    legend on plot

--- test/test_rotation_pid.py
+++ test/test_rotation_pid.py
@@ -9,22 +9,40 @@ from controls.controllers.PIDController import PID
 def simple_system(val, u):
     return val + u
 
-def quadratic_system(val, u):
-    return val**2 + u
+def linear_system(val, u):
+    return .3 * val + 2 * u
 
+def neg_linear_system(val, u):
+    return -val + u
+
+def sine_system(val, u):
+    return val * np.sin(val) + u
+
+def sine_sys2(val, u):
+    return val * np.sin(val * u)
+
+def sqrt_system(val, u):
+    return (val) ** .5 + u
+
+def log_system(val, u):
+    return np.log(val + u)
+
+# unstable system?
 def cubic_system(val, u):
     return (val + u)**3
 
+# unstable system?
 def exponential_sys(val, u):
     return 2**(val - u)
 
 def test(system, p, i, d, sat):
     goal = 100
-    pid = PID(p, i, d, 15, thresh=.1, setpoint=goal)
+    pid = PID(p, i, d, sat, thresh=.1, setpoint=goal)
 
+    hz = 50
     val = 5
-    t = 1
-    big_number = 1000
+    t = 0
+    max_time = 5
 
     val_arr = [val]
 
@@ -33,31 +51,36 @@ def test(system, p, i, d, sat):
             print("Value overflowed")
             plt.plot(val_arr)
             plt.plot(np.full(len(val_arr), goal))
+            plt.legend(['state values', 'setpoint'])
             plt.show()
             return
-        if t > big_number:
+        if t > max_time:
             print("PID did not converge to setpoint in time")
-            plt.plot(val_arr)
-            plt.plot(np.full(len(val_arr), goal))
+            l = len(val_arr)
+            plt.plot(val_arr[:l])
+            plt.plot(np.full(l, goal))
+            plt.legend(['state values', 'setpoint'])
             plt.show()
             return
 
-        # system: x[t+1] = x[t] + u[t]
         u = pid.calculate(val, t)
         val = system(val, u)
-        t += 1
+        t += 1/hz
         val_arr.append(val)
 
     print("yay PID worked")
     print(f"error: {abs(pid.setpoint - val)}")
     plt.plot(val_arr)
     plt.plot(np.full(len(val_arr), goal))
+    plt.legend(['state values', 'setpoint'])
     plt.show()
 
 if __name__ == "__main__":
     # test(simple_system, 1.5, 0, 0, 15)
-    test(quadratic_system, -100, 0, 0, 1000)
-    # test(cubic_system, -1, 0, 0, 15)
-    # test(exponential_sys, 0, 0, 0, 15)
+    test(linear_system, .5, .1, 0, 1000)
+    # test(neg_linear_system, -.1, 0, -100, 1000)
+    # test(sqrt_system, 10000, 0, 0, 1)
+    # test(sine_system, 1, 0, 0, 15)
+    # test(sine_sys2, 10, 0, 0, 15)
     
 # TODO: tune PID gains for cubic and exponential system
